447d514e43a66ffc34e0b44992b72eaccce6dc07
figonzal1 <Felipe.gonzalezalarcon94@gmail.com>
2020-05-03 21:14:23 -0400

    Evalua 2 modulo 5
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

---
+++ app/src/main/java/cl/figonzal/evaluatool/evalua2/modulo5/Ortografia.java
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------------------
+                                                                              -
+ This file is subject to the terms and conditions defined in                  -
+ file 'LICENSE', which is part of this source code package                    -
+                                                                              -
+ Autor: Felipe González                                                       -
+ Email: felipe.gonzalezalarcon94@gmail.com                                    -
+                                                                              -
+ Copyright (c) 2020                                                           -
+                                                                              -
+ Last modified 03-05-20 21:14                                                 -
+ -----------------------------------------------------------------------------*/
+
+package cl.figonzal.evaluatool.evalua2.modulo5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ortografia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ortografia);
+    }
+}
