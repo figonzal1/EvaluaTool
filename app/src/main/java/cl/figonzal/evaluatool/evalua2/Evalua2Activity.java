df7f21fb1b28ad83cd1ba99153a0e7bbd94859be
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2020-07-05 17:15:44 -0400

    ResolucionProblemasE2M6 para evalua 2 modulo 6
    Signed-off-by: figonzal1 <Felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/figonzal/evaluatool/evalua2/Evalua2Activity.java
+++ app/src/main/java/cl/figonzal/evaluatool/evalua2/Evalua2Activity.java
@@ -8,7 +8,7 @@
                                                                               -
  Copyright (c) 2020                                                           -
                                                                               -
- Last modified 05-07-20 16:17                                                 -
+ Last modified 05-07-20 17:08                                                 -
  -----------------------------------------------------------------------------*/
 
 package cl.figonzal.evaluatool.evalua2;
@@ -44,6 +44,7 @@ import cl.figonzal.evaluatool.evalua2.modulo3.NivelesAdaptacionE2M3;
 import cl.figonzal.evaluatool.evalua2.modulo4.ComprensionLectoraE2M4;
 import cl.figonzal.evaluatool.evalua2.modulo5.OrtografiaE2M5;
 import cl.figonzal.evaluatool.evalua2.modulo6.CalculoNumeracionE2M6;
+import cl.figonzal.evaluatool.evalua2.modulo6.ResolucionProblemasE2M6;
 import cl.figonzal.evaluatool.interfaces.AbrirActivity;
 import cl.figonzal.evaluatool.modelo.Evalua;
 import io.github.luizgrp.sectionedrecyclerviewadapter.SectionAdapter;
@@ -99,6 +100,7 @@ public class Evalua2Activity extends AppCompatActivity implements EvaluaAdapter.
 
         List<Evalua> subItems6 = new ArrayList<>();
         subItems6.add(new Evalua(getString(R.string.EVALUA_2_M6_SI_1)));
+        subItems6.add(new Evalua(getString(R.string.EVALUA_2_M6_SI_2)));
 
         //HEADERS
         sectionedRecyclerViewAdapter.addSection(new EvaluaAdapter(getString(R.string.EVALUA_2_MODULO_1), subItems1, this));
@@ -213,6 +215,13 @@ public class Evalua2Activity extends AppCompatActivity implements EvaluaAdapter.
                         CalculoNumeracionE2M6.class,
                         getString(R.string.SUB_ITEM_CLICK),
                         getString(R.string.CLICK_CAL_NUMERACION)
+                );
+            } else if (sectionedRecyclerViewAdapter.getPositionInSection(itemAdapterPosition) == 1) {
+                abrirActividad(
+                        this,
+                        ResolucionProblemasE2M6.class,
+                        getString(R.string.SUB_ITEM_CLICK),
+                        getString(R.string.CLICK_CAL_RES_PROBLEMAS)
                 );
             }
         }
