f006da0a7c5d716a562ec6bc4892b9f8e52b99a1
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2021-03-06 02:06:29 -0300

    ViewBinding en evalua 4
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
    
    Took 1 hour 46 minutes

--- app/src/main/java/cl/figonzal/evaluatool/evalua/evalua4/modulo2/PensamientoAnalogicoE4M2.kt
+++ app/src/main/java/cl/figonzal/evaluatool/evalua/evalua4/modulo2/PensamientoAnalogicoE4M2.kt
@@ -6,9 +6,9 @@
  Autor: Felipe González
  Email: felipe.gonzalezalarcon94@gmail.com
 
- Copyright (c) 2020
+ Copyright (c) 2021
 
- Last modified 29-11-20 14:47
+ Last modified 06-03-21 2:04
  */
 package cl.figonzal.evaluatool.evalua.evalua4.modulo2
 
@@ -17,12 +17,11 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
-import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import cl.figonzal.evaluatool.R
+import cl.figonzal.evaluatool.databinding.ActivityPensamientoAnalogicoE4M2Binding
 import cl.figonzal.evaluatool.dialogs.CorregidoDialogFragment
 import cl.figonzal.evaluatool.interfaces.EvaluaInterface
 import cl.figonzal.evaluatool.utilidades.EvaluaUtils
@@ -38,6 +37,7 @@ class PensamientoAnalogicoE4M2 : AppCompatActivity(), EvaluaInterface {
         private const val MEDIA = 13.55
     }
 
+    private lateinit var binding: ActivityPensamientoAnalogicoE4M2Binding
     private val perc = arrayOf(
             arrayOf(20, 99),
             arrayOf(19, 97),
@@ -89,10 +89,10 @@ class PensamientoAnalogicoE4M2 : AppCompatActivity(), EvaluaInterface {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pensamiento_analogico_e4_m2)
+        binding = ActivityPensamientoAnalogicoE4M2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.include.toolbar)
 
         val actionBar = supportActionBar!!
         actionBar.setDisplayHomeAsUpEnabled(true)
@@ -111,30 +111,28 @@ class PensamientoAnalogicoE4M2 : AppCompatActivity(), EvaluaInterface {
 
         //Promedio y desviacion
         //TetView desviacion y media
-        val tvMedia = findViewById<TextView>(R.id.tv_media_value)
-        val tvDesviacion = findViewById<TextView>(R.id.tv_desviacion_value)
-        tvMedia.text = MEDIA.toString()
-        tvDesviacion.text = DESVIACION.toString()
+        binding.cardViewConstantes.tvMediaValue.text = MEDIA.toString()
+        binding.cardViewConstantes.tvDesviacionValue.text = DESVIACION.toString()
 
         //TAREA 1
-        tvSubTotalT1 = findViewById(R.id.tv_pd_subtotal_t1)
-        etAprobadasT1 = findViewById(R.id.et_aprobadas_t1)
-        etReprobadasT1 = findViewById(R.id.et_reprobadas_t1)
-        tvSubTotalT2 = findViewById(R.id.tv_pd_subtotal_t2)
-        etAprobadasT2 = findViewById(R.id.et_aprobadas_t2)
-        etReprobadasT2 = findViewById(R.id.et_reprobadas_t2)
+        tvSubTotalT1 = binding.tvPdSubtotalT1
+        etAprobadasT1 = binding.etAprobadasT1
+        etReprobadasT1 = binding.etReprobadasT1
+        tvSubTotalT2 = binding.tvPdSubtotalT2
+        etAprobadasT2 = binding.etAprobadasT2
+        etReprobadasT2 = binding.etReprobadasT2
 
         //TOTAL
-        tvPdTotal = findViewById(R.id.tv_pd_total_value)
-        tvPdCorregido = findViewById(R.id.tv_pd_total_corregido_value)
-        tvPercentil = findViewById(R.id.tv_percentil_value)
-        tvNivel = findViewById(R.id.tv_nivel_obtenido_value)
-        tvDesviacionCalculada = findViewById(R.id.tv_desviacion_calculada_value)
+        tvPdTotal = binding.tvPdTotalValue
+        tvPdCorregido = binding.cardViewFinal.tvPdTotalCorregidoValue
+        tvPercentil = binding.cardViewFinal.tvPercentilValue
+        tvNivel = binding.cardViewFinal.tvNivelObtenidoValue
+        tvDesviacionCalculada = binding.cardViewFinal.tvDesviacionCalculadaValue
 
-        progressBar = findViewById(R.id.progressBar)
+        progressBar = binding.cardViewFinal.progressBar
         progressBar.max = perc[0][1]
 
-        val ivCorregido = findViewById<ImageView>(R.id.iv_help_pd_corregido)
+        val ivCorregido = binding.cardViewFinal.ivHelpPdCorregido
         ivCorregido.setOnClickListener {
 
             Timber.i(getString(R.string.DIALOGO_AYUDA_MSG_ABIERTO))
@@ -144,7 +142,7 @@ class PensamientoAnalogicoE4M2 : AppCompatActivity(), EvaluaInterface {
             dialogFragment.show(supportFragmentManager, getString(R.string.DIALOGO_AYUDA))
         }
 
-        val tvBaremo = findViewById<TextView>(R.id.tvBaremo)
+        val tvBaremo = binding.tablaBaremo.tvBaremo
         EvaluaUtils.configurarTextoBaremo(supportFragmentManager, tvBaremo, perc, getString(R.string.TOOLBAR_PENSAMIENTO_ANALOGICO))
 
     }
