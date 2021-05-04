323e3678b81da28759280233762147a431cf7854
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2021-05-04 16:40:23 -0400

    Archivo de tablas baremos para evalua 7 y testings para evalua 7
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
    
    Took 36 minutes

---
+++ app/src/test/java/cl/figonzal/evaluatool/evalua/evalua7/modulo4/ComprensionLectoraE7M4Test.kt
@@ -0,0 +1,53 @@
+/*
+
+ This file is subject to the terms and conditions defined in
+ file 'LICENSE', which is part of this source code package
+
+ Autor: Felipe González
+ Email: felipe.gonzalezalarcon94@gmail.com
+
+ Copyright (c) 2021
+
+ Last modified 04-05-21 16:40
+ */
+package cl.figonzal.evaluatool.evalua.evalua7.modulo4
+
+import cl.figonzal.evaluatool.baremosTables.comprensionLectoraE7M4Baremo
+import cl.figonzal.evaluatool.utilidades.Utils.calcularDesviacion
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class ComprensionLectoraE7M4Test(private val pd_total: Double, private val percentil_esperado: Double,
+                                 private val desviacion_esperada: Double) {
+    private var m1: ComprensionLectoraE7M4? = null
+
+    @Before
+    fun setUp() {
+        m1 = ComprensionLectoraE7M4()
+    }
+
+    @Test
+    fun testCalcularPercentil() {
+        assertEquals(percentil_esperado, m1!!.calculatePercentile(pd_total).toDouble(), 0.01)
+    }
+
+    @Test
+    fun testCalcularDesviacion() {
+        assertEquals(desviacion_esperada, calcularDesviacion(MEDIA, DESVIACION, pd_total, false), 0.01)
+    }
+
+    companion object {
+        private const val DESVIACION = 6.71
+        private const val MEDIA = 14.52
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Array<Array<Any>> {
+            return comprensionLectoraE7M4Baremo()
+        }
+    }
+}
