afd0137592677b23347c9704a6a40937e2fa59a1
Felipe Gonzalez Alarcon <felipe.gonzalezalarcon94@gmail.com>
2021-07-20 10:04:55 -0400

    Splash screen
    Signed-off-by: Felipe Gonzalez Alarcon <Felipe.gonzalezalarcon94@gmail.com>
    
    Took 1 hour 23 minutes

---
+++ app/src/main/java/cl/figonzal/evaluatool/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+/*
+
+ This file is subject to the terms and conditions defined in
+ file 'LICENSE', which is part of this source code package
+
+ Autor: Felipe González
+ Email: felipe.gonzalezalarcon94@gmail.com
+
+ Copyright (c) 2021
+
+ Last modified 20-07-21 10:01
+ */
+package cl.figonzal.evaluatool
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(
+            Intent(
+                this@SplashScreenActivity,
+                MainActivity::class.java
+            ).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        )
+        finish()
+    }
+}
