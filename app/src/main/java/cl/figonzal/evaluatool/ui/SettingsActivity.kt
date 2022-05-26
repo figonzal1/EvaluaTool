db9df2d4d4de83153a1c1863e5fa8664a289d3f9
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2022-05-25 23:29:14 -0400

    Politica de privacidad en settings y mejoras de UI en preferencias
    Signed-off-by: Felipe González Alarcón <felipe.gonzalezalarcon94@gmail.com>
    
    Took 1 minute

--- app/src/main/java/cl/figonzal/evaluatool/ui/SettingsActivity.kt
+++ app/src/main/java/cl/figonzal/evaluatool/ui/SettingsActivity.kt
@@ -8,12 +8,14 @@
 
  Copyright (c) 2022
 
- Last modified 28/2/22 1:00
+ Last modified 25-05-22 23:11
  */
 
 package cl.figonzal.evaluatool.ui
 
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -27,6 +29,7 @@ import cl.figonzal.evaluatool.service.FirebaseService
 import cl.figonzal.evaluatool.utils.configureActionBar
 import cl.figonzal.evaluatool.utils.toast
 import timber.log.Timber
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -55,9 +58,17 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             //Put app version un summary preferences
-            findPreference<Preference>(resources.getString(R.string.shared_pref_version_app))?.apply {
-                summary =
-                    "${resources.getString(R.string.app_name)} Versión: ${BuildConfig.VERSION_NAME}"
+            findPreference<Preference>(resources.getString(R.string.SHARED_PREF_VERSION))?.apply {
+                summary = BuildConfig.VERSION_NAME
+            }
+
+            findPreference<Preference>(getString(R.string.SHARED_PREF_PRIVACY_POLICY))?.setOnPreferenceClickListener {
+
+                Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(getString(R.string.PRIVACY_POLICY_URL))
+                    startActivity(this)
+                }
+                true
             }
         }
 
