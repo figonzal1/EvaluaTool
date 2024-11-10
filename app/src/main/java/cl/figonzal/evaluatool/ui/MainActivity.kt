935b423b52b4a833e7af794ae9888193051ad128
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-11-09 22:37:57 -0300

    fix politica de privacidad
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/figonzal/evaluatool/ui/MainActivity.kt
+++ app/src/main/java/cl/figonzal/evaluatool/ui/MainActivity.kt
@@ -8,7 +8,7 @@
 
  Copyright (c) 2024
 
- Last modified 09-11-24, 22:36
+ Last modified 09-11-24, 22:37
  */
 package cl.figonzal.evaluatool.ui
 
@@ -37,6 +37,7 @@ import cl.figonzal.evaluatool.ui.evaluas.evalua8.Evalua8Activity
 import cl.figonzal.evaluatool.ui.evaluas.evalua9.Evalua9Activity
 import cl.figonzal.evaluatool.utils.SharedPrefUtil
 import cl.figonzal.evaluatool.utils.handleNightMode
+import cl.figonzal.evaluatool.utils.handlePrivacyPolicy
 import cl.figonzal.evaluatool.utils.mainActivityTo
 import cl.figonzal.evaluatool.utils.setUpAnimations
 import cl.figonzal.evaluatool.utils.setUpCardViewCustomCorners
@@ -101,7 +102,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
             }
 
-            //handlePrivacyPolicy(sharedPrefUtil)
+            handlePrivacyPolicy(sharedPrefUtil)
         }
     }
 
